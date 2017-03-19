1daf588cfccb36bd928e62f7bf2d5302e4d5f5e8
Dmitriy@ITAdapter <dmitriy@itadapter.com>
2017-03-18 20:29:42 -0400

    add stuff

---
+++ PD.Business/SocialTrading/IUserStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NFX.DataAccess.Distributed;
+
+
+namespace PD.Contracts.SocialTrading
+{
+  /// <summary>
+  /// Denotes a contract for the entoty that acts like a store for social user trading
+  /// The implementation must be thread-safe for parallel user processing
+  /// </summary>
+  public interface IUserStore
+  {
+    /// <summary>
+    /// Puts user into the store. Returns true if was added, otherwise replaces the existing one
+    /// </summary>
+    bool Put(User user);
+
+    /// <summary>
+    /// Fetches user from the store by global ID or null if not found
+    /// </summary>
+    User Get(GDID gUser);
+
+    /// <summary>
+    /// Deletes the specified user from the store identified by global id.
+    /// Returns true if found and deleted
+    /// </summary>
+    bool Remove(GDID gUser);
+
+    /// <summary>
+    /// Returns the count of users in the store
+    /// </summary>
+    long Count { get; }
+
+    /// <summary>
+    /// Add the trade to user object returning it, or null if user was not found
+    /// </summary>
+    User AcceptTrade(GDID gUser, User.Trade trade);
+  }
+}
---
+++ PD.Business/SocialTrading/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NFX;
+using NFX.Collections;
+using NFX.DataAccess.Distributed;
+
+namespace PD.Contracts.SocialTrading
+{
+  public class User
+  {
+    public class Trade
+    {
+      private Trade() { }
+      public Trade(GDID id, int prov, long provId, string sym, decimal val, string msg)
+      {
+        ID = id;
+        Provider = prov;
+        ProviderID = provId;
+        TimestampUTC = App.TimeSource.UTCNow;
+        Symbol = sym;
+        Value = val;
+        Message = msg;
+      }
+      public GDID     ID          { get;  private set; }
+      public int      Provider    { get;  private set; }
+      public long     ProviderID  { get;  private set; }
+      public DateTime TimestampUTC{ get;  private set; }
+      public string   Symbol      { get;  private set; }
+      public decimal  Value       { get;  private set; }
+      public string   Message     { get;  private set; }
+    }
+
+    public const int MAX_TRADE_COUNT = 256;
+
+    private User(){ }
+    public User(GDID id, string name, DateTime dob)
+    {
+      ID = id;
+      Name = name ?? "<anonymous>";
+      DOB = dob;
+      m_Trades = new List<Trade>(16);
+    }
+
+    private List<Trade> m_Trades;
+
+    public GDID      ID          { get; private set; }
+    public string    Name        { get; private set; }
+    public DateTime  DOB         { get; private set; }
+    public StringMap SocialMsg   { get; set; }
+    public ulong     BuyerScore  { get; set; }
+    public ulong     SellerScore { get; set; }
+    public GDID?     GVendor     { get; set; }
+    public bool      IsVendor    { get; set; }
+    public bool?     PayoutApproved { get; set; }
+    public bool?     ReturnApproved { get; set; }
+    public IEnumerable<Trade> Trades  { get { return m_Trades; } }
+    public float     Age
+    {
+      get { return (float)(App.TimeSource.Now - DOB).TotalDays / 365f; }
+    }
+
+    public void AddTrade(Trade trade)
+    {
+      if (m_Trades.Count == MAX_TRADE_COUNT)
+        m_Trades.RemoveAt(0);
+
+      m_Trades.Add(trade);
+    }
+
+  }
+}
